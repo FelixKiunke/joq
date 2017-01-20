f681b26b0253a6bcd15d62041a7f1472760084d3
Felix Kiunke <dev@fkiunke.de>
2017-01-20 18:36:33 +0100

    Fix default config

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,5 @@ use Mix.Config
 # NOTE: This config is only used for this project, it's not inherited when you use this library within an app.
 
 if Mix.env == :test or Mix.env == :dev do
-  config :joq,
-    retry_strategy: {:immediately, 2}
+  config :joq, retry: {:immediately, 2}
 end
